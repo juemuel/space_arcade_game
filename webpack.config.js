37c077de870f1e4c5cc41912dc762492e88bb348
juemuel <799525644@qq.com>
2023-05-28 00:29:36 +0800

    fix img src路径问题

--- webpack.config.js
+++ webpack.config.js
@@ -23,33 +23,16 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|svg|jpg|gif|wav)$/,
                 use: {
-                    loader:'file-loader',
-                    options:{
-                        // name表示原始名，hash表示hash值，ext表示资源后缀
-                        name: '[name].[ext]',
-                        // 打包后到存放路径，
-                        outputPath:'assets/',
-                        // 可直接访问的路径
-                        publicPath:'assets/',
-                    }
+                  loader:'file-loader',
+                  options:{
+                    name: '[name].[ext]', // name表示原始名，hash表示hash值，ext表示资源后缀
+                    outputPath:'assets/',// 打包后到存放路径，
+                    publicPath:'assets/',// 可直接访问的路径
+                  }
                 },
-            },
-            {
-                test: /\.wav$/,
-                use: {
-                    loader:'file-loader',
-                    options:{
-                        // name表示原始名，hash表示hash值，ext表示资源后缀
-                        name: '[name].[ext]',
-                        // 打包后到存放路径、src中引用的url路径
-                        outputPath:'assets/',
-                        // 可直接访问的路径
-                        publicPath:'assets/',
-                    }
-                },
-            }
+              },
         ]
     }
 };
